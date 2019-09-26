b0e6f2b5d6f62624d0759c4ea1fd32cfd6e43328
larsvens <larsvens@kth.se>
2019-09-26 17:13:00 +0200

    added version0 steering controller

--- common/scripts/ctrl_interface.py
+++ common/scripts/ctrl_interface.py
@@ -9,6 +9,7 @@ import rospy
 from common.msg import Trajectory
 from fssim_common.msg import Cmd
 from fssim_common.msg import State as fssimState
+from visualization_msgs.msg import Marker
 
 class CtrlInterface:
     def __init__(self):
@@ -17,7 +18,8 @@ class CtrlInterface:
         self.trajstarsub = rospy.Subscriber("trajstar", Trajectory, self.trajstar_callback)
         self.vehicle_out_sub = rospy.Subscriber("/fssim/base_pose_ground_truth", fssimState, self.vehicle_out_callback)
         self.vehicleinpub = rospy.Publisher('/fssim/cmd', Cmd, queue_size=10)
-        self.rate = rospy.Rate(10)
+        self.lhptpub = rospy.Publisher('/lhpt_vis', Marker, queue_size=1)
+        self.rate = rospy.Rate(10) # TMP!
 
         # set static vehicle params
         self.setStaticParams()
@@ -27,37 +29,88 @@ class CtrlInterface:
         self.vehicle_out = fssimState()
         self.trajstar = Trajectory()
 
-        # wait for messages before entering main loop # tmp commented out!!
+        # wait for messages before entering main loop
         while(not self.trajstar.Fyf):
             print("waiting for trajstar")
             self.rate.sleep()
 
         # main loop
-        while not rospy.is_shutdown():
-            
-            # compute delta corresponding to Fyf request (linear tire, Rajamani) 
-            Fyf_request = self.trajstar.Fyf[0]
-            print "Fyf_request = ", str(Fyf_request)
-            alpha_f = Fyf_request/(2*self.Cf)
+        while not rospy.is_shutdown(): 
+            X = self.vehicle_out.x
+            Y = self.vehicle_out.y
+            psi = self.vehicle_out.yaw
             psidot = self.vehicle_out.r
             vx = self.vehicle_out.vx
             vy = self.vehicle_out.vy
-
-            theta_f = np.arctan2(vy+self.lf*psidot,vx)
-
-            if(self.vehicle_out.vx > 1.0):
-                delta = theta_f + alpha_f 
-            else:
-                delta = 0
-            print "alpha_f = ", str(alpha_f)
-            print "theta_f = ", str(theta_f)
-            print "delta_out = ", str(delta)
             
-            self.vehicle_in.delta = 0.85*delta
-            #self.vehicle_in.Fyf = self.trajstar.Fyf[0]
-            self.vehicle_in.dc = 0.00001*self.trajstar.Fx[0]
-            #self.vehicle_in.header.stamp = rospy.Time.now()
+            # compute local curvature of trajhat (rho)
+            lhpt_idx = 5;
+            lhpt = {"X": self.trajstar.X[lhpt_idx], "Y": self.trajstar.Y[lhpt_idx]}
+            deltaX = (lhpt["X"]-X)
+            deltaY = (lhpt["Y"]-Y)
+            lh_dist = np.sqrt(deltaX**2 + deltaY**2)
+            lh_angle = np.arctan2(deltaY,deltaX) - psi 
+            rho = 2*np.sin(lh_angle)/lh_dist
+
+
+
+            
+            # compute delta corresponding to Fyf request (linear tire, Rajamani) 
+            Fyf_request = self.trajstar.Fyf[0]
+            alpha_f = Fyf_request/(2*self.Cf)
+
+            # compute angle of velocity vector at center of front axle 
+            if(vx < 1.0):
+                theta_Vf = np.arctan2(vy+self.lf*psidot,1.0)
+            else:
+                theta_Vf = np.arctan2(vy+self.lf*psidot,vx)
+
+
+
+
+            # compute control
+            delta_out = rho*(self.lf + self.lr) # kinematic feed fwd
+            dc_out = 0.00005*self.trajstar.Fx[0]
+            
+            # prints 
+            print ""
+            print "params"
+            print "lf =     ", str(self.lf)
+            print "Cf       ", str(self.Cf)
+            print ""
+            print "state vars"
+            print "vx =     ", str(vx)
+            print "vy =     ", str(vy)
+            print "psidot = ", str(psidot)
+            print ""            
+            print "computed vars"
+            print "theta_Vf = ", str(theta_Vf)
+            print "Fyf_request = ", str(Fyf_request)
+            print "alpha_f_request = ", str(alpha_f)
+            print "delta_out = ", str(delta_out) 
+            print "dc_out = ", str(dc_out)                    
+
+            # publish ctrl cmd            
+            self.vehicle_in.delta = delta_out
+            self.vehicle_in.dc = dc_out
             self.vehicleinpub.publish(self.vehicle_in)
+            
+            # publish visualization
+            m = Marker()
+            m.header.stamp = rospy.Time.now()
+            m.header.frame_id = "map"
+            m.pose.position.x = lhpt["X"];
+            m.pose.position.y = lhpt["Y"];
+            m.pose.position.z = 0.1;
+            m.type = m.SPHERE;
+            m.scale.x = 0.6;
+            m.scale.y = 0.6;
+            m.scale.z = 0.6;
+            m.color.a = 1.0; 
+            m.color.r = 0.0;
+            m.color.g = 0.0;
+            m.color.b = 1.0;
+            self.lhptpub.publish(m)
 
     def trajstar_callback(self, msg):
         self.trajstar = msg
@@ -66,8 +119,9 @@ class CtrlInterface:
         self.vehicle_out = msg
 
     def setStaticParams(self):
-        self.Cf = 1000000 # Todo compute from magic formula
+        self.Cf = 1000000  # Todo compute from magic formula
         self.lf = rospy.get_param('/car/kinematics/b_F')
+        self.lr = rospy.get_param('/car/kinematics/b_R')
 
 if __name__ == '__main__':
     vm = CtrlInterface()
--- common/scripts/ctrl_interface_old.py
+++ common/scripts/ctrl_interface_old.py
@@ -7,26 +7,28 @@
 import numpy as np
 import rospy
 from common.msg import Trajectory
-from common.msg import VehicleIn
-from common.msg import VehicleOut
-from common.msg import StaticVehicleParams
+from fssim_common.msg import Cmd
+from fssim_common.msg import State as fssimState
 
 class CtrlInterface:
     def __init__(self):
         # init node subs pubs
         rospy.init_node('ctrl_interface', anonymous=True)
         self.trajstarsub = rospy.Subscriber("trajstar", Trajectory, self.trajstar_callback)
-        self.vehicle_out_sub = rospy.Subscriber("vehicle_out", VehicleOut, self.vehicle_out_callback)
-        self.vehicleinpub = rospy.Publisher('vehicle_in', VehicleIn, queue_size=10)
-        self.rate = rospy.Rate(100)
+        self.vehicle_out_sub = rospy.Subscriber("/fssim/base_pose_ground_truth", fssimState, self.vehicle_out_callback)
+        self.vehicleinpub = rospy.Publisher('/fssim/cmd', Cmd, queue_size=10)
+        self.rate = rospy.Rate(10) # TMP!
 
         # set static vehicle params
         self.setStaticParams()
 
         # init msgs
-        self.vehicle_in = VehicleIn()
-        self.vehicle_out = VehicleOut()
+        self.vehicle_in = Cmd()
+        self.vehicle_out = fssimState()
         self.trajstar = Trajectory()
+        
+        # misc
+        self.printcounter = 0 
 
         # wait for messages before entering main loop # tmp commented out!!
         while(not self.trajstar.Fyf):
@@ -38,20 +40,42 @@ class CtrlInterface:
             
             # compute delta corresponding to Fyf request (linear tire, Rajamani) 
             Fyf_request = self.trajstar.Fyf[0]
-            alpha_f = Fyf_request/(2*self.sp.Cr)
-            psidot = self.vehicle_out.psidot
+            alpha_f = Fyf_request/(2*self.Cf)
+            psidot = self.vehicle_out.r
             vx = self.vehicle_out.vx
             vy = self.vehicle_out.vy
-
-            if(self.vehicle_out.vx > 0.1):
-                delta = alpha_f + np.arctan2(vy+self.sp.lf*psidot,vx)
-            else:
-                delta = 0
             
-            self.vehicle_in.delta = delta
-            #self.vehicle_in.Fyf = self.trajstar.Fyf[0]
-            self.vehicle_in.Fx = self.trajstar.Fx[0]
-            self.vehicle_in.header.stamp = rospy.Time.now()
+            # compute angle of velocity vector at center of front axle 
+            if(vx < 1.0):
+                theta_Vf = np.arctan2(vy+self.lf*psidot,1.0)
+            else:
+                theta_Vf = np.arctan2(vy+self.lf*psidot,vx)
+
+            # compute controls
+            delta_out = 1.0*(theta_Vf + alpha_f) 
+            dc_out = 0.00006*self.trajstar.Fx[0]
+            
+            # prints 
+            print ""
+            print "params"
+            print "lf =     ", str(self.lf)
+            print "Cf       ", str(self.Cf)
+            print ""
+            print "state vars"
+            print "vx =     ", str(vx)
+            print "vy =     ", str(vy)
+            print "psidot = ", str(psidot)
+            print ""            
+            print "computed vars"
+            print "theta_Vf = ", str(theta_Vf)
+            print "Fyf_request = ", str(Fyf_request)
+            print "alpha_f_request = ", str(alpha_f)
+            print "delta_out = ", str(delta_out) 
+            print "dc_out = ", str(dc_out)                    
+
+            # publish ctrl cmd            
+            self.vehicle_in.delta = delta_out
+            self.vehicle_in.dc = dc_out
             self.vehicleinpub.publish(self.vehicle_in)
 
     def trajstar_callback(self, msg):
@@ -61,19 +85,8 @@ class CtrlInterface:
         self.vehicle_out = msg
 
     def setStaticParams(self):
-        self.sp = StaticVehicleParams()
-        self.sp.g = rospy.get_param('/g')
-        self.sp.l = rospy.get_param('/l')
-        self.sp.w = rospy.get_param('/w')
-        self.sp.m = rospy.get_param('/m')
-        self.sp.Iz = rospy.get_param('/Iz')
-        self.sp.lf = rospy.get_param('/lf')
-        self.sp.lr = rospy.get_param('/lr')
-        self.sp.Cf = rospy.get_param('/Cf')
-        self.sp.Cr = rospy.get_param('/Cr')
-        self.sp.Da = rospy.get_param('/Da')
-        self.sp.deltamin = rospy.get_param('/deltamin')
-        self.sp.deltamax = rospy.get_param('/deltamax')
+        self.Cf = 1000000  # Todo compute from magic formula
+        self.lf = rospy.get_param('/car/kinematics/b_F')
 
 if __name__ == '__main__':
     vm = CtrlInterface()
