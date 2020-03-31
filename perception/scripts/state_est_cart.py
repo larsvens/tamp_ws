b5b65dce6aef1771a1cdf6c0897355d634a2d958
larsvens <larsvens@kth.se>
2020-03-31 09:01:11 +0200

    moved tf bc to state_est_cart

--- perception/scripts/state_est_cart.py
+++ perception/scripts/state_est_cart.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
-# Descrition: Publishes state in cartesian coordinates 
+# Descrition: Publishes state in cartesian coordinatesand broadcasts tf tamp_map -> base_link
 # selects input topic based on sensor_setup param 
 # sensor_setup = "rhino_real": /OpenDLV/SensorMsgGPS & /OpenDLV/SensorMsgCAN
 # sensor_setup = "fssim": /fssim/base_pose_ground_truth
 
+
 #import numpy as np
+import time
+import utm
+import yaml
 import rospy
+import tf
+import rospkg
 from fssim_common.msg import State as fssimState
 from common.msg import State
 from common.msg import OriginPoseUTM
 from opendlv_ros.msg import SensorMsgGPS
 from opendlv_ros.msg import SensorMsgCAN 
-import utm
 
 class StateEstCart:
     # constructor
@@ -28,6 +33,11 @@ class StateEstCart:
 
         # init local vars
         self.state_out = State()
+
+        # load vehicle dimensions 
+        dimsyaml = rospkg.RosPack().get_path('common') + '/config/vehicles/' + self.robot_name + '/config/distances.yaml'
+        with open(dimsyaml, 'r') as f:
+            self.dims = yaml.load(f,Loader=yaml.SafeLoader)   
        
         # init subs pubs
         if (self.sensor_setup == "rhino_real"):
@@ -45,7 +55,8 @@ class StateEstCart:
             self.received_fssim_state = False
     
         self.statepub = rospy.Publisher('state_cart', State, queue_size=1)
-
+        self.tfbr = tf.TransformBroadcaster()
+        
         # wait for messages before entering main loop
         if (self.sensor_setup == "rhino_real"):
             while((not self.received_odlv_gps) or (not self.received_odlv_can)):
@@ -63,10 +74,34 @@ class StateEstCart:
 
         # Main loop
         while not rospy.is_shutdown():
+            
+            # timing
+            start = time.time()
+            
+            # state estimation
             if (self.sensor_setup == "rhino_real"):
                 self.update_rhino_state()
+
+            # broadcast tf
+            start_tfbc = time.time()
+            self.broadcast_dyn_tfs()
+            self.broadcast_static_tfs()
+            end_tfbc = time.time()
+            comptime_tfbc = end_tfbc-start_tfbc
+
+
+            # timing: check wrt dt
+            end = time.time()
+            comptime = end-start
+            if (comptime > self.dt):
+                rospy.logwarn("state_est_cart: compute time exceeding dt!")
+                rospy.logwarn("state_est_cart: total comptime =        " + str(comptime))
+                rospy.logwarn("state_est_cart: comptime tf broadcast = " + str(comptime_tfbc))
             
             self.rate.sleep() 
+
+
+
 
     def update_rhino_state(self):
         X, Y, utm_nr, utm_letter = utm.from_latlon(self.odlv_gps_msg.lat, self.odlv_gps_msg.long)
@@ -75,6 +110,64 @@ class StateEstCart:
         if(utm_nr != self.origin_pose_utm.utm_nr):
             rospy.logerr("UTM zone mismatch: GPS measurement utm_nr =     " + str(utm_nr) + ", origin_pose utm_nr =     " + str(self.origin_pose_utm.utm_nr))
             rospy.logerr("UTM zone mismatch: GPS measurement utm_letter = " + utm_letter + ", origin_pose utm_letter = " + str(chr(self.origin_pose_utm.utm_letter)))
+
+
+    def broadcast_dyn_tfs(self):
+        self.tfbr.sendTransform((self.state_out.X, self.state_out.Y, 0),
+                                tf.transformations.quaternion_from_euler(0, 0, self.state_out.psi),
+                                rospy.Time.now(),
+                                "base_link",
+                                "tamp_map")
+        
+        # todo - steering angle here
+#        self.tfbr.sendTransform((self.dims["left_steering_hinge"]["left_front_wheel"]["x"], self.dims["left_steering_hinge"]["left_front_wheel"]["y"], self.dims["left_steering_hinge"]["left_front_wheel"]["z"]),
+#                                tf.transformations.quaternion_from_euler(0, 0, 1.0),
+#                                rospy.Time.now(),
+#                                "left_front_wheel",
+#                                "left_steering_hinge") 
+#
+#        self.tfbr.sendTransform((self.dims["right_steering_hinge"]["right_front_wheel"]["x"], self.dims["right_steering_hinge"]["right_front_wheel"]["y"], self.dims["right_steering_hinge"]["right_front_wheel"]["z"]),
+#                                tf.transformations.quaternion_from_euler(0, 0, 1.0),
+#                                rospy.Time.now(),
+#                                "right_front_wheel",
+#                                "right_steering_hinge") 
+
+    def broadcast_static_tfs(self):
+        self.tfbr.sendTransform((self.dims["base_link"]["cog"]["x"], self.dims["base_link"]["cog"]["y"], self.dims["base_link"]["cog"]["z"]),
+                                (0, 0, 0, 1), 
+                                rospy.Time.now(),
+                                "cog",
+                                "base_link")         
+
+        self.tfbr.sendTransform((self.dims["cog"]["chassis"]["x"], self.dims["cog"]["chassis"]["y"], self.dims["cog"]["chassis"]["z"]),
+                                (0, 0, 0, 1), 
+                                rospy.Time.now(),
+                                "chassis",
+                                "cog")  
+
+        self.tfbr.sendTransform((self.dims["chassis"]["left_rear_wheel_joint"]["x"], self.dims["chassis"]["left_rear_wheel_joint"]["y"], self.dims["chassis"]["left_rear_wheel_joint"]["z"]),
+                                (0, 0, 0, 1), 
+                                rospy.Time.now(),
+                                "left_rear_wheel_joint",
+                                "chassis") 
+
+        self.tfbr.sendTransform((self.dims["chassis"]["right_rear_wheel_joint"]["x"], self.dims["chassis"]["right_rear_wheel_joint"]["y"], self.dims["chassis"]["right_rear_wheel_joint"]["z"]),
+                                (0, 0, 0, 1), 
+                                rospy.Time.now(),
+                                "right_rear_wheel_joint",
+                                "chassis") 
+
+        self.tfbr.sendTransform((self.dims["chassis"]["left_steering_hinge_joint"]["x"], self.dims["chassis"]["left_steering_hinge_joint"]["y"], self.dims["chassis"]["left_steering_hinge_joint"]["z"]),
+                                (0, 0, 0, 1), 
+                                rospy.Time.now(),
+                                "left_steering_hinge",
+                                "chassis") 
+
+        self.tfbr.sendTransform((self.dims["chassis"]["right_steering_hinge"]["x"], self.dims["chassis"]["right_steering_hinge"]["y"], self.dims["chassis"]["right_steering_hinge"]["z"]),
+                                (0, 0, 0, 1), 
+                                rospy.Time.now(),
+                                "right_steering_hinge",
+                                "chassis") 
 
 
     def fssim_state_callback(self, msg):
--- perception/scripts/state_est_frenet.py
+++ perception/scripts/state_est_frenet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Descrition: Adds frenet coordinates to state and broadcasts tf
+# Descrition: Adds frenet coordinates to state
 
 # subscribes:
 # cartesian state from state_est_cart
@@ -8,21 +8,15 @@
 
 # publishes: 
 # state (topic /state) - full state containing both frenet and cartesian coordinates
-# broadcasts TF tamp_map -> base_link
+
 
 import numpy as np
 import rospy
 from common.msg import Path
-#from fssim_common.msg import State as fssimState
-#from opendlv_ros.msg import SensorMsgGPS
-#from opendlv_ros.msg import SensorMsgCAN 
 from common.msg import State
 from coordinate_transforms import ptsCartesianToFrenet
 from util import angleToInterval
 from util import angleToContinous
-import tf
-import rospkg
-import yaml
 import time
 
 class StateEstFrenet:
@@ -47,16 +41,10 @@ class StateEstFrenet:
         # init pubs
         self.statepub = rospy.Publisher('state', State, queue_size=10)
         self.state_out = State()        
-        self.tfbr = tf.TransformBroadcaster()
            
         # init local vars
         self.passed_halfway = False
         self.lapcounter = 0
-        
-        # load vehicle dimensions 
-        dimsyaml = rospkg.RosPack().get_path('common') + '/config/vehicles/' + self.robot_name + '/config/distances.yaml'
-        with open(dimsyaml, 'r') as f:
-            self.dims = yaml.load(f,Loader=yaml.SafeLoader)               
     
         # wait for messages before entering main loop
         while(not self.received_pathglobal):
@@ -88,21 +76,13 @@ class StateEstFrenet:
             end_statest = time.time()
             comptime_statest = end_statest-start_statest
             
-            # broadcast tf
-            start_tfbc = time.time()
-            self.broadcast_dyn_tfs()
-            self.broadcast_static_tfs()
-            end_tfbc = time.time()
-            comptime_tfbc = end_tfbc-start_tfbc
-
             # timing: check wrt dt
             end = time.time()
             comptime = end-start
             if (comptime > self.dt):
-                rospy.logwarn("state_est: compute time exceeding dt!")
-                rospy.logwarn("state_est: total comptime =        " + str(comptime))
-                rospy.logwarn("state_est: comptime statest =      " + str(comptime_statest))
-                rospy.logwarn("state_est: comptime tf broadcast = " + str(comptime_tfbc))
+                rospy.logwarn("state_est_frenet: compute time exceeding dt!")
+                rospy.logwarn("state_est_frenet: total comptime =        " + str(comptime))
+                rospy.logwarn("state_est_frenet: comptime statest =      " + str(comptime_statest))
             
             self.rate.sleep()   
             
@@ -149,62 +129,6 @@ class StateEstFrenet:
         self.state_out.deltapsi = self.state_out.deltapsi[0]
         self.state_out.header.stamp = rospy.Time.now()
 
-    def broadcast_dyn_tfs(self):
-        self.tfbr.sendTransform((self.state_out.X, self.state_out.Y, 0),
-                                tf.transformations.quaternion_from_euler(0, 0, self.state_out.psi),
-                                rospy.Time.now(),
-                                "base_link",
-                                "tamp_map")
-        
-        # todo - steering angle here
-#        self.tfbr.sendTransform((self.dims["left_steering_hinge"]["left_front_wheel"]["x"], self.dims["left_steering_hinge"]["left_front_wheel"]["y"], self.dims["left_steering_hinge"]["left_front_wheel"]["z"]),
-#                                tf.transformations.quaternion_from_euler(0, 0, 1.0),
-#                                rospy.Time.now(),
-#                                "left_front_wheel",
-#                                "left_steering_hinge") 
-#
-#        self.tfbr.sendTransform((self.dims["right_steering_hinge"]["right_front_wheel"]["x"], self.dims["right_steering_hinge"]["right_front_wheel"]["y"], self.dims["right_steering_hinge"]["right_front_wheel"]["z"]),
-#                                tf.transformations.quaternion_from_euler(0, 0, 1.0),
-#                                rospy.Time.now(),
-#                                "right_front_wheel",
-#                                "right_steering_hinge") 
-
-    def broadcast_static_tfs(self):
-        self.tfbr.sendTransform((self.dims["base_link"]["cog"]["x"], self.dims["base_link"]["cog"]["y"], self.dims["base_link"]["cog"]["z"]),
-                                (0, 0, 0, 1), 
-                                rospy.Time.now(),
-                                "cog",
-                                "base_link")         
-
-        self.tfbr.sendTransform((self.dims["cog"]["chassis"]["x"], self.dims["cog"]["chassis"]["y"], self.dims["cog"]["chassis"]["z"]),
-                                (0, 0, 0, 1), 
-                                rospy.Time.now(),
-                                "chassis",
-                                "cog")  
-
-        self.tfbr.sendTransform((self.dims["chassis"]["left_rear_wheel_joint"]["x"], self.dims["chassis"]["left_rear_wheel_joint"]["y"], self.dims["chassis"]["left_rear_wheel_joint"]["z"]),
-                                (0, 0, 0, 1), 
-                                rospy.Time.now(),
-                                "left_rear_wheel_joint",
-                                "chassis") 
-
-        self.tfbr.sendTransform((self.dims["chassis"]["right_rear_wheel_joint"]["x"], self.dims["chassis"]["right_rear_wheel_joint"]["y"], self.dims["chassis"]["right_rear_wheel_joint"]["z"]),
-                                (0, 0, 0, 1), 
-                                rospy.Time.now(),
-                                "right_rear_wheel_joint",
-                                "chassis") 
-
-        self.tfbr.sendTransform((self.dims["chassis"]["left_steering_hinge_joint"]["x"], self.dims["chassis"]["left_steering_hinge_joint"]["y"], self.dims["chassis"]["left_steering_hinge_joint"]["z"]),
-                                (0, 0, 0, 1), 
-                                rospy.Time.now(),
-                                "left_steering_hinge",
-                                "chassis") 
-
-        self.tfbr.sendTransform((self.dims["chassis"]["right_steering_hinge"]["x"], self.dims["chassis"]["right_steering_hinge"]["y"], self.dims["chassis"]["right_steering_hinge"]["z"]),
-                                (0, 0, 0, 1), 
-                                rospy.Time.now(),
-                                "right_steering_hinge",
-                                "chassis") 
         
     def state_cart_callback(self, msg):
         self.state_cart = msg
