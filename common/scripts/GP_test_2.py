1c15dfae003479ddaac26dcf537811f6b4378350
larsvens <lars.salevid@kth.se>
2020-12-14 18:12:05 +0100

    wip on friction est

---
+++ common/scripts/GP_test_2.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python2
+
+
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def kernel(x, y, l2=0.1, sigma_f=1.0):
+    sqdist = np.sum(x**2,1).reshape(-1,1) + \
+             np.sum(y**2,1) - 2*np.dot(x, y.T)
+    return sigma_f**2 * np.exp(-.5 * (1/l2) * sqdist)
+
+def generate_noisy_points(n=10, noise_variance=1e-6):
+    np.random.seed(777)
+    X = np.random.uniform(-3., 3., (n, 1))
+    y = 0.5 + 0.4*np.sin(X) + np.random.randn(n, 1) * noise_variance**0.5
+    return X, y
+
+def generate_noisy_points_2(n=10, noise_variance=1e-6):
+    np.random.seed(777)
+    X = np.random.uniform(-3., 5., (n, 1))
+    y = 0.5 + 0.4*np.sin(X) + np.random.randn(n, 1) * noise_variance**0.5 
+    return X, y
+
+def posterior(Xtrain, ytrain, Xtest, l2=0.1, noise_var_train=1e-6,sigma_f=1.0):
+    ytrain_zeromean = ytrain-np.mean(ytrain)
+    
+    # compute the mean at our test points.
+    Ntrain = len(Xtrain)
+    K = kernel(Xtrain, Xtrain, l2,sigma_f)
+    L = np.linalg.cholesky(K + noise_var_train*np.eye(Ntrain))
+    Lk = np.linalg.solve(L, kernel(Xtrain, Xtest, l2,sigma_f))
+    mean = np.dot(Lk.T, np.linalg.solve(L, ytrain_zeromean)) + np.mean(ytrain)
+    # compute the variance at our test points.
+    K_ = kernel(Xtest, Xtest, l2,sigma_f)
+    sd = np.sqrt(np.diag(K_) - np.sum(Lk**2, axis=0))
+    return (mean, sd, K)
+
+
+Xtrain, ytrain = generate_noisy_points()
+Xtest, ytest = generate_noisy_points_2(100)
+Xtest.sort(axis=0)
+mean, sd, K = posterior(Xtrain,ytrain, Xtest,3.5,0.01,0.25)
+print(mean, sd)
+
+
+plt.plot(Xtrain, ytrain, 'xb')
+plt.plot(Xtest, mean.flatten()-1.96*sd,'k--' )
+plt.plot(Xtest, mean.flatten()+1.96*sd,'k--' )
+
+
+#plt.plot(Xtest, ytest, 'xr')
+plt.show()
+
+#plt.figure()
+#plt.plot(x, f(x), 'r:', label=r'$f(x) = x\,\sin(x)$')
+#plt.plot(x, y_pred, 'b-', label='Prediction')
+#plt.fill(np.concatenate([x, x[::-1]]),
+#         np.concatenate([y_pred - 1.9600 * sigma,
+#                        (y_pred + 1.9600 * sigma)[::-1]]),
+#         alpha=.5, fc='b', ec='None', label='95% confidence interval')
+    
--- perception/scripts/friction_est.py
+++ perception/scripts/friction_est.py
@@ -22,14 +22,23 @@ import rospy
 from common.msg import State
 from common.msg import Trajectory
 from common.msg import MuEst
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32
 class FrictionEstimation:
     # constructor
     def __init__(self):
         # init node subs pubs
         rospy.init_node('friction_estimation', anonymous=True)
         self.rate = rospy.Rate(10)  # approx 10hz update rate of live plot
-        
+
+        # params
+        self.N_buffer = rospy.get_param('/N_buffer_fe')
+        self.idx_lh = rospy.get_param('/idx_lh')
+        self.mu_mean_nominal = rospy.get_param('/mu_nominal')
+        self.mu_sigma_nominal = rospy.get_param('/mu_sigma_nominal')
+        self.Ff_util = rospy.get_param('/Ff_util')
+        self.Fr_util = rospy.get_param('/Fr_util')
+
+        # subs & pubs        
         self.state_sub = rospy.Subscriber("/state", State, self.state_callback)
         self.state = State()
         self.received_state = False
@@ -40,13 +49,11 @@ class FrictionEstimation:
         
         self.mu_est_pub = rospy.Publisher("/mu_est", MuEst, queue_size=1)
         self.mu_est = MuEst()
+        self.mu_est.mu_est_mean = np.zeros(self.N_buffer)
+        self.mu_est.mu_est_sigma = np.zeros(self.N_buffer)
+                
+        self.rho_diff_pub = rospy.Publisher("/rho_diff", Float32, queue_size=1)
         
-        # test
-        self.test_pub = rospy.Publisher("/test_est", Float32MultiArray, queue_size=1)
-        
-        # params
-        self.N_buffer = rospy.get_param('/N_buffer_fe')
-        self.idx_lh = rospy.get_param('/idx_lh')
         
         # init node vars
         self.s_perceived_buffer = np.zeros(self.N_buffer)
@@ -56,14 +63,27 @@ class FrictionEstimation:
         self.s_planned_buffer = np.zeros(self.N_buffer*2) # 
         self.rhof_planned_buffer = np.zeros(self.N_buffer*2)
         self.rhor_planned_buffer = np.zeros(self.N_buffer*2)
+        self.rhof_planned = np.zeros(self.N_buffer)
         
         # init live plot
         self.do_live_plot = True
         if self.do_live_plot:
             fig = plt.figure(num=None, figsize=(24, 12), dpi=80, facecolor='w', edgecolor='k')
             ax = fig.add_subplot(111)
-            rho_perceived_ln, = ax.plot(np.zeros(self.N_buffer),'b.',markersize=20)
-#            rho_planned_ln    = ax.plot(np.zeros(self.N_buffer),'r.',markersize=20)
+            line_objs = ax.plot(np.arange(self.N_buffer),np.zeros(self.N_buffer),'b.',
+                                 np.arange(self.N_buffer),np.zeros(self.N_buffer),'r.',
+                                 np.arange(self.N_buffer),np.zeros(self.N_buffer),'k--', 
+                                 np.arange(self.N_buffer),np.zeros(self.N_buffer),'k--',ms = 20,lw = 5)
+
+            rho_perceived_ln = line_objs[0]
+            rho_planned_ln = line_objs[1]
+            conf_intv_ub = line_objs[2]
+            conf_intv_lb = line_objs[3]
+            
+            ax.legend(['perceived','planned','conf_itv'])
+            ax.set_xlabel('s (m)')
+            ax.set_ylabel('mu')
+            
             ax.set_ylim([0,1])
             plt.ion()
             plt.show()  
@@ -77,21 +97,34 @@ class FrictionEstimation:
             rospy.loginfo_throttle(1, "friction_est: waiting for trajectory")
             rospy.sleep(0.1)
 
-        rospy.loginfo("friction_est: started") # no main loop - everything happens in state callback
+        rospy.loginfo("friction_est: started") 
         
-
-
-        # live plot
+        # main loop - only live plot
         t = time.time()
         while not rospy.is_shutdown():
             
             if self.do_live_plot:
+                x_axis = self.s_perceived_buffer-self.state.s
+                
+                rho_perceived_ln.set_xdata(x_axis)
                 rho_perceived_ln.set_ydata(self.rhof_perceived_buffer)
-                rho_perceived_ln.set_xdata(np.arange(len(self.rhof_perceived_buffer)))
-#                rho_planned_ln.set_ydata(self.self.rhof_planned)
-#                rho_planned_ln.set_xdata(np.arange(len(self.rhof_planned)))
+                
+                rho_planned_ln.set_xdata(x_axis) 
+                rho_planned_ln.set_ydata(self.rhof_planned) 
+                
+                rospy.loginfo("friction_est: len x: " + str(len(x_axis)))
+                y_axis = np.array(self.mu_est.mu_est_mean) + 1.96*np.array(self.mu_est.mu_est_sigma)
+                rospy.loginfo("friction_est: max y: " + str(np.max(y_axis)))
+                
+                conf_intv_ub.set_xdata(x_axis)
+                conf_intv_ub.set_ydata(self.mu_est.mu_est_mean + 1.96*np.array(self.mu_est.mu_est_sigma))
+                
+                conf_intv_lb.set_xdata(x_axis)
+                conf_intv_lb.set_ydata(self.mu_est.mu_est_mean - 1.96*np.array(self.mu_est.mu_est_sigma))
                 
                 plt.pause(0.0001)
+                ax.set_xlim([np.min(self.s_perceived_buffer)-np.max(self.s_perceived_buffer), 0.1])
+                
                 rospy.loginfo("friction_est: looptime plot update: " + str(time.time() - t))
                 t = time.time()
                 
@@ -103,47 +136,63 @@ class FrictionEstimation:
         
         if(self.state.vx > 1.0): # avoid issues at low speed (also we wont have useful data at very low speeds)
         
+            # set the prior (TODO SHIFT FWD INSTEAD OF RESETTING)
+            mu_est_mean_prior = self.mu_mean_nominal*np.ones(self.N_buffer) 
+            mu_est_sigma_prior = self.mu_sigma_nominal*np.ones(self.N_buffer)
+            
             # update buffers
-            self.s_perceived_buffer = np.roll(self.s_perceived_buffer,1)
-            self.s_perceived_buffer[0] = self.state.s
-            self.rhof_perceived_buffer = np.roll(self.rhof_perceived_buffer,1)
-            self.rhof_perceived_buffer[0] = self.state.rhof
-            self.rhor_perceived_buffer = np.roll(self.rhor_perceived_buffer,1)
-            self.rhor_perceived_buffer[0] = self.state.rhor
+            self.s_perceived_buffer = np.roll(self.s_perceived_buffer,-1)
+            self.s_perceived_buffer[-1] = self.state.s
+            self.rhof_perceived_buffer = np.roll(self.rhof_perceived_buffer,-1)
+            self.rhof_perceived_buffer[-1] = self.state.rhof
+#            self.rhor_perceived_buffer = np.roll(self.rhor_perceived_buffer,1)
+#            self.rhor_perceived_buffer[0] = self.state.rhor
             
             s_lh = self.trajstar.s[self.idx_lh]
             rhof_lh = np.sqrt(self.trajstar.Fxf[self.idx_lh]**2 + self.trajstar.Fyf[self.idx_lh]**2)/self.trajstar.Fzf[self.idx_lh]
-            rhor_lh = np.sqrt(self.trajstar.Fxr[self.idx_lh]**2 + self.trajstar.Fyr[self.idx_lh]**2)/self.trajstar.Fzr[self.idx_lh]
-            self.s_planned_buffer = np.roll(self.s_planned_buffer,1)
-            self.s_planned_buffer[0] = s_lh
-            self.rhof_planned_buffer = np.roll(self.rhof_planned_buffer,1)
-            self.rhof_planned_buffer[0] = rhof_lh
-            self.rhor_planned_buffer = np.roll(self.rhor_planned_buffer,1)
-            self.rhor_planned_buffer[0] = rhor_lh
+            #rhor_lh = np.sqrt(self.trajstar.Fxr[self.idx_lh]**2 + self.trajstar.Fyr[self.idx_lh]**2)/self.trajstar.Fzr[self.idx_lh]
+            self.s_planned_buffer = np.roll(self.s_planned_buffer,-1)
+            self.s_planned_buffer[-1] = s_lh
+            self.rhof_planned_buffer = np.roll(self.rhof_planned_buffer,-1)
+            self.rhof_planned_buffer[-1] = rhof_lh
+            #self.rhor_planned_buffer = np.roll(self.rhor_planned_buffer,1)
+            #self.rhor_planned_buffer[0] = rhor_lh
             
-            # if buffers are filled
+            # interp if buffers are filled
             if (np.count_nonzero(self.s_perceived_buffer) == self.s_perceived_buffer.size and
                 np.count_nonzero(self.s_planned_buffer) == self.s_planned_buffer.size):
+                
+                # error if interpolation intervals do not overlap
+                mgn = 0.5   
+                if (self.s_perceived_buffer[0] < self.s_planned_buffer[0]-mgn):
+                    rospy.logerr("friction est: improper overlap: s_perceived_buffer[0] = " + str(self.s_perceived_buffer[0]) + ", s_planned_buffer[0] = " + str(self.s_planned_buffer[0]))
+                if (self.s_perceived_buffer[-1] > self.s_planned_buffer[-1]+mgn):
+                    rospy.logerr("friction est: improper overlap: s_perceived_buffer[-1] = " + str(self.s_perceived_buffer[-1]) + ", s_planned_buffer[-1] = " + str(self.s_planned_buffer[-1]))
+                                
+                
                 # interpolate planned 
                 self.rhof_planned = np.interp(self.s_perceived_buffer, self.s_planned_buffer, self.rhof_planned_buffer)
                 #rhor_planned = np.interp(self.s_perceived_buffer, self.s_planned_buffer, self.rhor_planned_buffer)
-            
+
+                # pub rho diff 
+                msg = Float32()
+                msg.data = self.rhof_planned[-1] - self.rhof_perceived_buffer[-1]
+                self.rho_diff_pub.publish(msg)
+           
                 
                 # do mu est (tmp)
                 mgn = 0.1
                 self.mu_est.s = self.s_perceived_buffer
-                self.mu_est.mu_est_mean = np.ones(self.N_buffer) # TMP
+                self.mu_est.mu_est_mean = mu_est_mean_prior
                 for i in range(self.N_buffer):
-                    if(self.rhof_perceived_buffer[i] < self.rhof_planned[i] - mgn ):
+                    if(self.rhof_perceived_buffer[i] < self.rhof_planned[i]*self.Ff_util - mgn ):
                         self.mu_est.mu_est_mean[i] = self.rhof_perceived_buffer[i]
                     
-                self.mu_est.mu_est_sigma = np.zeros(self.N_buffer)
+                self.mu_est.mu_est_sigma = mu_est_sigma_prior
                 self.mu_est.header.stamp = rospy.Time.now()
                 self.mu_est_pub.publish(self.mu_est)
                
-                testmsg = Float32MultiArray()
-                testmsg.data = np.random.rand(10)
-                self.test_pub.publish(testmsg)
+
 
 
                 
